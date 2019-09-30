5d4649cae53ffccfaceb7d2b48038db1180ba1b5
bodykhalil <36100776+bodykhalil@users.noreply.github.com>
2019-09-30 10:08:14 +0200

    added cascade relation

--- database_setup.py
+++ database_setup.py
@@ -4,9 +4,10 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
- 
+
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -18,42 +19,43 @@ class User(Base):
 
 class Category(Base):
     __tablename__ = 'category'
-    
 
-    name =Column(String(200), nullable = False)
-    id = Column(Integer, primary_key = True)
-    user_id = Column(Integer,ForeignKey('user.id'))
-    user = relationship(User) 
+    name = Column(String(200), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
     @property
     def serialize(self):
-       
-       return {
-           'name'         : self.name,
-           'id'         : self.id,
-       }
+
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class Item(Base):
     __tablename__ = 'item'
-    
 
-    name =Column(String(200), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(200), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String)
-    category_id  = Column(Integer,ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    user_id   = Column(Integer,ForeignKey('user.id'))
-    user = relationship(User) 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User ,cascade="save-update, merge, delete")
 
     @property
     def serialize(self):
-       
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-       }
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+        }
+
 
 engine = create_engine('sqlite:///itemcat.db')
- 
+
 
 Base.metadata.create_all(engine)
